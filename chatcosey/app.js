e3419a4485216b6a30533105bcee0da109e3c787
shunnyjang <suuuu101@naver.com>
2021-09-25 18:18:15 +0900

    Fix cors bug
    cors를 잘못 선언하고 있어서 request와 response가 전송되지 않던 버그 해결

--- chatcosey/app.js
+++ chatcosey/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
-// Setup CORS
-app.use(require('cors'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Setup CORS
+app.use(require('cors')());
+
+//Bring in the routes
 app.use("/user", require("./routes/user"));
 app.use("/chatroom", require("./routes/chatroom"));
 
